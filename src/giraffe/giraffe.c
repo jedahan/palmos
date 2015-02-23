61613567bc64caca39eca962a14b93c5297c7fc4
Jonathan Dahan <jonathan@jonathan.is>
2015-02-23 16:46:27 -0500

    HUGE refactoring for multiple forms

--- src/giraffe/giraffe.c
+++ src/giraffe/giraffe.c
@@ -12,7 +12,7 @@ const void DEBUGBOX(char *ARGSTR1, char *ARGSTR2) {
   char buf[MAX_STRING_LENGTH];
   int l=0;
   l+=StrPrintF(buf+l, "%s %s:%d\n", __FUNCTION__, __FILE__, __LINE__);
-  FrmCustomAlert(alertInfo, buf, ARGSTR1, ARGSTR2);
+  FrmCustomAlert(Alert, buf, ARGSTR1, ARGSTR2);
 }
 
 const void showKey(Char letter){
@@ -30,6 +30,8 @@ typedef struct {
 } Sprite;
 
 typedef Sprite *SpritePtr;
+
+Sprite nextLetter = {20, 120, 1, 0, NULL};
 
 const void updateSprite(SpritePtr sprite){
   sprite->x += sprite->dx;
@@ -51,65 +53,109 @@ const void drawSprite(SpritePtr sprite){
   WinDrawChars(sprite->letter, 1, sprite->x, sprite->y);
 }
 
+static Boolean GraffitiFormHandleEvent(EventPtr event){
+  Boolean handled = false;
+  switch (event->eType) {
+    case frmOpenEvent:
+      FrmDrawForm(FrmGetActiveForm());
+      handled = true;
+      break;
+    case keyDownEvent:
+      if(event->data.keyDown.chr == sentence[offset]){
+        offset++;
+        if(offset == StrLen(sentence)){ FrmGotoForm(WinForm); }
+      }
+      handled = true;
+      break;
+    case nilEvent: {
+      if(offset < StrLen(sentence)){
+        updateSprite(&nextLetter);
+        drawSprite(&nextLetter);
+      }
+      handled = true;
+      break;
+    }
+    default:
+      break;
+  }
+  return handled;
+}
+
+static Boolean WinFormHandleEvent(EventPtr event){
+  Boolean handled = false;
+  switch (event->eType) {
+    case frmOpenEvent:
+      FrmDrawForm(FrmGetActiveForm());
+      handled = true;
+      break;
+    default:
+      break;
+  }
+  return handled;
+}
+
+static Boolean AppHandleEvent(EventPtr event){
+  FormPtr pfrm;
+  Int16 formId;
+  Boolean handled = false;
+  switch (event->eType){
+    case frmLoadEvent:
+      formId = event->data.frmLoad.formID;
+      pfrm = FrmInitForm(formId);
+      FrmSetActiveForm(pfrm);
+      switch (formId){
+        case GraffitiForm:
+          FrmSetEventHandler(pfrm, GraffitiFormHandleEvent);
+          handled = true;
+          break;
+        case WinForm:
+          FrmSetEventHandler(pfrm, WinFormHandleEvent);
+          handled = true;
+          break;
+        default:
+          break;
+      }
+      break;
+    case menuEvent:
+      MenuEraseStatus(NULL);
+      switch (event->data.menu.itemID) {
+        case MainOptionsAboutCmd:
+          FrmAlert(HelpAlert);
+          handled = true;
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+  return handled;
+}
+
+static void AppEventLoop(void){
+  EventType event;
+  short error;
+  do {
+    EvtGetEvent(&event, 100);
+    if (MenuHandleEvent(NULL, &event, &error)) { continue; }
+    if (AppHandleEvent(&event)){ continue; }
+    if (SysHandleEvent(&event)) { continue; }
+    FrmDispatchEvent(&event);
+  } while (event.eType != appStopEvent);
+}
+
 UInt32 PilotMain(UInt16 cmd, MemPtr cmdPBP, UInt16 launchFlags) {
   short err;
   EventType e;
-  FormType *pfrm;
   UInt16 obj;
   FieldType *pfld;
-  Sprite nextLetter = {20, 120, 1, 0, NULL};
 
   // Make sure only react to NormalLaunch, not Reset, Beam, Find, GoTo...
   if (cmd == sysAppLaunchCmdNormalLaunch) {
-    FrmGotoForm(Giraffe);
-    while(1){
-      EvtGetEvent(&e, 100);
-      if (SysHandleEvent(&e)) { continue; }
-      if (MenuHandleEvent(NULL, &e, &err)) { continue; }
-      switch (e.eType) {
-
-        case keyDownEvent:
-          if(e.data.keyDown.chr == sentence[offset]){
-            offset++;
-            if(offset == StrLen(sentence)){ showKey('!'); }
-          }
-          break;
-
-        case ctlSelectEvent:
-          if (e.data.ctlSelect.controlID == Ok) {
-            goto _quit;
-	  }
-          goto _default;
-          break;
-
-        case frmLoadEvent:
-          FrmSetActiveForm(FrmInitForm(e.data.frmLoad.formID));
-          break;
-
-        case frmOpenEvent:
-          pfrm = FrmGetActiveForm();
-          FrmDrawForm(pfrm);
-          break;
-
-        case appStopEvent:
-          goto _quit;
-
-        case nilEvent:
-          if(offset < StrLen(sentence)){
-            updateSprite(&nextLetter);
-            drawSprite(&nextLetter);
-          }
-        default:
-        _default:
-          if (FrmGetActiveForm())
-            FrmHandleEvent(FrmGetActiveForm(), &e);
-      }
-    }
-
-    _quit:
-      FrmCloseAllForms();
-						
+    FrmGotoForm(GraffitiForm);
+    AppEventLoop();
+    FrmCloseAllForms();
   }
   return 0;
 }
-
--- src/giraffe/giraffe.h
+++ src/giraffe/giraffe.h
@@ -1,7 +1,11 @@
-#define Giraffe 	1000
-#define GraffitiField	1501
-#define LastLetter 	2000
-#define Ok 		9999
-#define Alert	 	1000
-#define Help 		1000
-#define alertInfo 	3000	
+#define GraffitiForm 	1000
+#define LastLetter 	  1001
+
+#define MainMenu 2000
+#define MainOptionsAboutCmd 2001
+
+#define WinForm 		3000
+
+#define Ok 		    9999
+#define Alert 9010
+#define HelpAlert 9011
